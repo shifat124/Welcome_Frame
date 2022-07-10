d53f0438b15dba3825874ec82790c924068d2be2
Md. Shifat Laskor <69003347+shifat124@users.noreply.github.com>
2022-07-11 00:30:17 +0600

    Initial commit

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+public class MainClass {
+    public static void main(String[] args) {
+          MainFrame myFrame = new MainFrame();
+          myFrame.init();
+    }
+    
+    
+}
